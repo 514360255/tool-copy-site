aba6dceef78d9410a2b4a518dd41d86634044d1a
sunshine <jiuling.guo@bizatmobile.com>
2020-04-23 15:58:59 +0800

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -23,16 +23,85 @@ const mkdir = (file = '', mkPath = __dirname) => {
     !fs.existsSync(dirPath) && fs.mkdirSync(dirPath);
 };
 
+/**
+ * 拷贝以html为结尾的文件
+ * @param url
+ * @param encoding
+ */
 const requestUrl = (url, encoding = 'utf-8') => {
     request({url, encoding: null}, (error, response) => {
         if(error) return console.error('获取失败');
         if (response.statusCode === 200) {
             const str = iconv.decode(response.body, encoding).toString();
             const config = stub.config;
-            const file = path.resolve(__dirname, `../../${config.outDir}`);
-            console.log(file);
+            const dir = path.resolve(__dirname, `../../${config.outDir}`);
+            const urlMatch = url.match(/(([0-9A-z]+)\.html)/);
+            const fileName = urlMatch ? urlMatch[0] : 'index.html';
+            writeFile(`${dir}/${fileName}`, str);
+
+            // 下载图片
+            uploadImages(str, url, dir);
+
         }
     })
+};
+
+/**
+ * 下载图片
+ * @param str
+ * @param url
+ */
+const uploadImages = (str, url, dir) => {
+    const imgArr = str.match(/<img.*?(?:>|\/>)/gi);
+    const srcReg = /src=[\'\"]?([^\'\"]*)[\'\"]?/i;
+    let imgPathArr = [];
+    let isHost = false;
+    imgPathArr = distinct(imgPathArr);
+
+    if(/\/$/.test(url)) isHost = true;
+    for(let key in imgArr) {
+        let path = imgArr[key].match(srcReg)[1];
+        if(isHost) {
+            path = path.substr(1, path.length - 1);
+        }
+        const fileName = path.match(/([A-z0-9\-_\.]+\.(png|jpg|gif))$/)[0];
+        imgPathArr.push({
+            path: url + path,
+            fileName: fileName,
+            folderList: path.split('/').slice(0, -1)
+        });
+    }
+    imgPathArr.forEach(item => {
+        let catalog = '';
+        (item.folderList || []).forEach(folder => {
+            mkdir(folder, dir + catalog);
+            catalog = catalog + '/' + folder;
+            console.log(folder);
+            request({url: item.path, encoding: null}).pipe(fs.createWriteStream(dir + catalog, item.fileName));
+        });
+    })
+};
+
+/**
+ * 数组去重
+ * @param arr
+ * @returns {any[]}
+ */
+const distinct = arr => {
+    return Array.from(new Set(arr));
+};
+
+/**
+ * 写入到本地文件
+ * @param file
+ * @param data
+ */
+const writeFile = (file, data) => {
+    try {
+        fs.writeFileSync(file, data, {flag: 'w'});
+    }catch(e) {
+        throw('写入失败');
+    }
 };
 
 const utils = {
